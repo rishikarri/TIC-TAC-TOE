f047a5c597b46c689b1ec1fbf78e47f0f08163ce
Rishi Karri <rishikarri1@gmail.com>
2016-12-08 10:44:03 -0500

    Initial commit of tic tac toe game

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// 1. set up the board
+// 2. add an x or o 
+// put an onclick in the first square
+// when the user clicks, call function that puts an X int ht e box that was clicked
+// MILESTONE
+
+
+
+function markSquare(square){
+	console.log(square);
+	// I receive the whole div from this
+
+	//change the square to X onclick
+	square.innerHTML = "X";
+
+}
