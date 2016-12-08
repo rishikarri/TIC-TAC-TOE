e04e66e7be5bca6691552e969960ea983a314e9d
Rishi Karri <rishikarri1@gmail.com>
2016-12-08 10:51:58 -0500

    Added a random generator for the computer's move

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@
 // put an onclick in the first square
 // when the user clicks, call function that puts an X int ht e box that was clicked
 // MILESTONE
+// 3. Program computer response - need a random letter and a random number
 
 
 
@@ -12,5 +13,18 @@ function markSquare(square){
 
 	//change the square to X onclick
 	square.innerHTML = "X";
+
+}
+
+
+//computer move
+//1. Generate random number and letter for computer 
+//2. Need to make sure the comptuer doesn't go where you have already gone
+
+function computerMove(){
+	//generate a random  number from 1 to 3
+	var randomRow = Math.ceil(Math.random() * 3)
+	console.log(randomRow);
+
 
 }
