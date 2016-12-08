563023fd69c4c7c04aba1f13c3466013d0a67d36
Rishi Karri <rishikarri1@gmail.com>
2016-12-08 11:05:08 -0500

    Got the game to play between two players :o

--- scripts.js
+++ scripts.js
@@ -4,15 +4,28 @@
 // when the user clicks, call function that puts an X int ht e box that was clicked
 // MILESTONE
 // 3. Program computer response - need a random letter and a random number
+// 4. keep track of who's turn it is. 
+// 5. When a square is clicked, put the symbol and change who's turn it is
 
 
 
-function markSquare(square){
-	console.log(square);
+var whosTurn = 1;
+ // whosturn to player1
+
+
+function markSquare(currentSquare){
+	console.log(currentSquare);
 	// I receive the whole div from this
 
 	//change the square to X onclick
-	square.innerHTML = "X";
+
+	if (whosTurn === 1){
+		currentSquare.innerHTML = "X";
+		whosTurn = 2; 
+	}else{
+		currentSquare.innerHTML = "O";
+		whosTurn = 1;
+	}
 
 }
 
@@ -23,8 +36,19 @@ function markSquare(square){
 
 function computerMove(){
 	//generate a random  number from 1 to 3
-	var randomRow = Math.ceil(Math.random() * 3)
+	var randomRow = Math.ceil(Math.random() * 3);
 	console.log(randomRow);
+
+	//generate a random letter from a - c
+	var randomLetterNumberFormat = Math.ceil(Math.random() * 3);
+	// create an object that provides access to the letters based on the random number generated
+	var lettersObject = {
+		1: "A",
+		2: "B", 
+		3: "C"
+	};
+
+	
 
 
 }
